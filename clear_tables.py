15069bed389d3f35611b25f1ac046b7d5774f470
Isaac Chaljub <isaac.95@hotmail.com>
2025-04-06 11:24:25 +0200

    Cleared tables, added clear_tables.py

---
+++ clear_tables.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+def clear_tables():
+    """Clear all data from the database tables"""
+    conn = sqlite3.connect('solar_crowdfunding.db')
+    c = conn.cursor()
+    
+    # Clear tables in the correct order to respect foreign key constraints
+    c.execute('DELETE FROM dividends')
+    c.execute('DELETE FROM shareholders')
+    c.execute('DELETE FROM projects')
+    
+    conn.commit()
+    conn.close()
+    print("All tables have been cleared successfully!")
+
+if __name__ == "__main__":
+    clear_tables() 
